a4c639539cc3c434cba191b8243cb877b168f278
Moupiyali Banerjee <164043422+codedbyMoupiyali@users.noreply.github.com>
2026-04-18 17:11:04 +0530

    Add files via upload

---
+++ day64.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Graph {
+    int numVertices;
+    struct Node** adjLists;
+};
+
+struct Node* createNode(int v) {
+    struct Node* newNode = malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Graph* createGraph(int vertices) {
+    struct Graph* graph = malloc(sizeof(struct Graph));
+    graph->numVertices = vertices;
+    graph->adjLists = malloc(vertices * sizeof(struct Node*));
+    for (int i = 0; i < vertices; i++) {
+        graph->adjLists[i] = NULL;
+    }
+    return graph;
+}
+
+void addEdge(struct Graph* graph, int s, int d) {
+    struct Node* newNode = createNode(d);
+    newNode->next = graph->adjLists[s];
+    graph->adjLists[s] = newNode;
+}
+
+void BFS(struct Graph* graph, int startVertex) {
+    int* visited = calloc(graph->numVertices, sizeof(int));
+    int* queue = malloc(graph->numVertices * sizeof(int));
+    int front = 0, rear = 0;
+
+    visited[startVertex] = 1;
+    queue[rear++] = startVertex;
+
+    printf("BFS traversal order: ");
+
+    while (front < rear) {
+        int currentVertex = queue[front++];
+        printf("%d ", currentVertex);
+
+        struct Node* temp = graph->adjLists[currentVertex];
+        while (temp) {
+            int adjVertex = temp->vertex;
+            if (!visited[adjVertex]) {
+                visited[adjVertex] = 1;
+                queue[rear++] = adjVertex;
+            }
+            temp = temp->next;
+        }
+    }
+    printf("\n");
+    free(visited);
+    free(queue);
+}
+
+int main() {
+    int n, m, startVertex;
+    if (scanf("%d %d", &n, &m) != 2) return 0;
+
+    struct Graph* graph = createGraph(n);
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        addEdge(graph, u, v);
+    }
+
+    scanf("%d", &startVertex);
+
+    BFS(graph, startVertex);
+
+    return 0;
+}
